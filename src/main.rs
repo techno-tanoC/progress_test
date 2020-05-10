74c23e5ed696c812d5418dcebcdbe7d01b4ca567
techno-tanoC <develop.tanoc@gmail.com>
2020-05-10 15:59:48 +0900

    Revert "file"
    This reverts commit 75108e03651215a5299052612ab7cf97f5dc3b91.

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,15 @@ async fn main() {
     let pg_clone = pg.clone();
 
     let download_handle = tokio::task::spawn(async move {
-        let stream = tokio::fs::File::open("ubuntu.iso").await.unwrap();
+        let url = "https://releases.ubuntu.com/20.04/ubuntu-20.04-desktop-amd64.iso";
+        let res = reqwest::get(url).await.unwrap();
+
+        let mut stream = res
+            .bytes_stream()
+            .map_err(|e| futures::io::Error::new(futures::io::ErrorKind::Other, e))
+            .into_async_read()
+            .compat();
+
         let (mut reader, mut writer) = (BufReader::new(stream), BufWriter::new(pg_clone));
         io::copy(&mut reader, &mut writer).await.unwrap();
     });
